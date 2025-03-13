1325de071dc2d1f0960ffb8c5e8911b4d2414701
Inixio <inixio@Ordenador-portatil-de-Inixio.local>
2025-03-13 13:57:55 +0100

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   devToolbar: {
     enabled: false,
   },
